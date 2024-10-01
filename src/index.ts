05605106c79ec7343bb275f452f531fe71b46a20
spblue4422 <spblue4422@naver.com>
2024-10-02 02:39:02 +0900

    get method name

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,46 @@ getHtmls();
 
 async function getHtmls() {
   try {
-    let urlArr: string[] = [];
-    let apiNameArr: string[][] = [];
+    let urlArr: string[] = [
+      "https://urllib3.readthedocs.io/en/stable/reference/urllib3.poolmanager.html",
+      "https://urllib3.readthedocs.io/en/stable/reference/urllib3.response.html",
+      "https://urllib3.readthedocs.io/en/stable/reference/urllib3.fields.html",
+      "https://urllib3.readthedocs.io/en/stable/reference/urllib3.util.html",
+    ];
 
-    const baseHtml: AxiosResponse = await axios.get(
-      "https://urllib3.readthedocs.io/en/stable/reference/index.html"
-    );
+    // const baseHtml: AxiosResponse = await axios.get(
+    //   "https://urllib3.readthedocs.io/en/stable/reference/index.html"
+    // );
 
-    const $ = cheerio.load(baseHtml.data);
+    // const $ = cheerio.load(baseHtml.data);
 
-    $("li.toctree-l1").each((i, e) => {
-      urlArr.push($(e).find("a").first().attr("href"));
-    });
+    // $("li.toctree-l1").each((i, e) => {
+    //   urlArr.push($(e).find("a").first().attr("href"));
+    // });
 
-    urlArr = urlArr
-      .slice(9, 17)
-      .map(
-        (url) => "https://urllib3.readthedocs.io/en/stable/reference/" + url
-      );
+    // urlArr = urlArr
+    //   .slice(9, 17)
+    //   .map(
+    //     (url) => "https://urllib3.readthedocs.io/en/stable/reference/" + url
+    //   );
 
     urlArr.forEach(async (url, idx) => {
-      const html: AxiosResponse = await axios.get(url);
-      const $ = cheerio.load(html.data);
+      const subHtml: AxiosResponse = await axios.get(url);
+      const subData = cheerio.load(subHtml.data);
       let tmpArr: string[] = [];
 
-      $("span.descname").each((i, el) => {
-        tmpArr.push($(el).text());
+      subData(".py.method").each((i, el) => {
+        tmpArr.push(cheerio.load(el)("dt").first().attr("id"));
+      });
+
+      subData(".py.function").each((i, el) => {
+        tmpArr.push(cheerio.load(el)("dt").first().attr("id"));
       });
 
       console.log(tmpArr);
-      apiNameArr.push(tmpArr);
-    });
 
-    // console.log(apiNameArr);
+      // url이랑 뽑아낸 배열로 gpt api로 질문하는 부분
+    });
   } catch (error) {
     console.log(error);
   }
